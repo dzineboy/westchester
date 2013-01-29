dbc16c5920cac39a1df8e060007b7532ea8ba058
Daniel Scott <emaildan@mac.com>
2013-01-29 15:55:33 -0500

    init cms files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WestchesterCms::Application.config.secret_token = '5f18feb9b406ecb018caac97073c7748d019b8dfa9333670dfb8d76023773d7fb0b51559524f46a108e64d991664f96b3411d4d143c7f10f9557e45210e9b800'
